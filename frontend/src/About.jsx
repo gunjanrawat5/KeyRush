234862a1826f730f0c1388a0d8728ecc172679a6
Natalie <naragon@stevens.edu>
2025-11-18 13:52:42 -0500

    Added About page description

--- frontend/src/About.jsx
+++ frontend/src/About.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div style = {{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '80vh', 
+      testAlign: 'center', 
+      padding: '0 20px' 
+    }}> 
+
+    <p style={{ maxWidth: '600px', fontSize: '1.2rem', lineHeight: '1.6' }}> 
+      Welcome to KeyRush, in this application users will be able to use this practice typing tool, designed to help improve thier typing speed and accuracy. When the user begins to type, a timer will start automatically and will measure their words per minute (WPM). This feature will help users track their progress throughout time. Since many job roles requires strong typing skills. This application was created to help out users build confidence and improve their typing performance, and overall professionalism. 
+
+      
+
+    </p>  
+   </div>
+  );
+};
 
 export default About
