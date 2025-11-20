8cbf936ba1903ec34c8d8941d74e8e3262e25269
Gunjan <gunjanrawat5@gmail.com>
2025-11-20 01:03:02 -0500

    Implemented timer

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Typing from './Typing'
 
 const Home = () => {
+  const [seconds,setSeconds] = useState(30)
+  useEffect(()=>{
+      if (seconds<=0) return
+      const interval = setInterval(()=>{
+        setSeconds(prev => prev-1)
+      },1000)
+      return () => clearInterval(interval);
+    },[seconds])
   
   return (
-    <div className=' flex justify-center mt-80 text-[#ffffff44] text-2xl font-[Quantico]'>
-      <Typing></Typing>
+    <div className=' text-[#fffffffb] text-2xl font-[Quantico]'>
+
+      <div className=' flex flex-col justify-center items-center mt-80 text-[#ffffff44]'>
+        <button className='text-white'>{seconds}</button>
+        <Typing></Typing>
+      </div>
+      
     </div>
   )
 }
